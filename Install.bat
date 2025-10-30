046b609e2accdf4bfc81d2a0cc75cd42cfa1a336
ageorge95 <arteni.george.daniel@gmail.com>
2025-10-30 15:14:01 +0200

    added the skeleton of the setup

---
+++ Install.bat
@@ -0,0 +1,41 @@
+@echo off
+
+set python=%1
+IF "%python%"=="" set python="python"
+
+:: installation
+
+git submodule update --progress --init --recursive --force
+
+%python% -m venv venv
+:: Windows doesn't allow the creation of symlinks without special priviledges, so hardlinks are created instead.
+mklink /h activate.bat venv\Scripts\activate.bat
+
+call activate.bat
+
+python install_helper.py || goto :error
+goto :all_good
+
+: error
+pause
+exit
+
+: all_good
+
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+
+:: post-installation message
+
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo waitress_pypi install complete.
+echo.
+echo Run 'activate' to activate waitress Python virtual environment and
+echo 'deactivate' to deactivate it.
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+pause
+
+deactivate
